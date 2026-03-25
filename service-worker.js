073aa4f17192cc5d1efbc7ffa62b52e7c7b1f294
nkanipandi-ops <nkanipandi@gmail.com>
2026-03-25 12:54:56 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,13 @@
-const CACHE_NAME = "weather-app-v1";
-
 self.addEventListener("install", (event) => {
-    console.log("Service Worker Installed");
+  console.log("SW Installed");
+  self.skipWaiting();
+});
 
-    event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll([
-                "/",
-                "/index.html",
-                "/style.css",
-                "/script.js",
-                "/icon.png"
-            ]);
-        })
-    );
+self.addEventListener("activate", (event) => {
+  console.log("SW Activated");
+  event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener("fetch", (event) => {
-    event.respondWith(
-        fetch(event.request).catch(() => caches.match(event.request))
-    );
+  event.respondWith(fetch(event.request));
 });
